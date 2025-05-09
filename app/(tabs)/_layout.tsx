26d3b5334232cc1f8ffe9b15946495bd630fb9d3
Alix Muller <alixmuller.am@gmail.com>
2025-05-09 21:48:35 +0900

    page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -40,6 +40,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="message.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="menu"
+        options={{
+          title: 'Weekly Menu',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="message.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
