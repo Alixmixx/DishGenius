9a3d84bebd8d6a0673c9ce8b5b46b63953865585
Alix Muller <alixmuller.am@gmail.com>
2025-05-09 21:52:00 +0900

    Menu page

---
+++ app/(tabs)/menu.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+import { MenuScreen } from '@/components/MenuScreen';
+import { ThemedView } from '@/components/ThemedView';
+
+/**
+ * Chat tab screen that displays the ChatBot interface
+ */
+export default function MenuTab() {
+  return (
+    <ThemedView style={styles.container}>
+      <MenuScreen />
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
